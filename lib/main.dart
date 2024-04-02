ad1bf92701d84aa47ec6132df48e6449f1c01541
minyeong <minyk0512@gmail.com>
2024-04-03 01:45:45 +0900

    Container 연습

--- lib/main.dart
+++ lib/main.dart
@@ -3,41 +3,32 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(actions: [
-        IconButton(
-          icon: Icon(Icons.home),
-          onPressed: () {
-            print('Tab!');
-          },
-        ),
-        Icon(Icons.play_arrow)
-      ], centerTitle: false, title: Text('This is AppBar')),
-      body: TestWidget(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.bug_report),
-        onPressed: () {
-          print('FAB Tab');
-        },
-      ),
-    ),
+        appBar: AppBar(title: Text('Study to Container')),
+        body: Center(child: CustomContainer())),
   ));
 }
 
-class TestWidget extends StatelessWidget {
-  const TestWidget({super.key});
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Center(
-        child: Text(
-          'Hello, Flutter',
-          style: TextStyle(
-            fontSize: 40,
-            color: Colors.black,
-          ),
-        ),
-      ),
-    );
+    return Container(
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(
+            color: Color(0x90013429),
+            border: Border.all(
+                color: Colors.red, width: 5, style: BorderStyle.solid),
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                offset: Offset(6, 10),
+                blurRadius: 10,
+                spreadRadius: 10
+              )
+            ]),
+        child: Center(child: Text('Container')));
   }
 }
