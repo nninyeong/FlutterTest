ede5f93dbf0eb4d0090b8eb6ca65ca5f3219f69c
minyeong <minyk0512@gmail.com>
2024-04-03 02:32:51 +0900

    Column 연습

--- lib/main.dart
+++ lib/main.dart
@@ -3,32 +3,30 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-        appBar: AppBar(title: Text('Study to Container')),
-        body: Center(child: CustomContainer())),
+      appBar: AppBar(
+        title: Center(child: Text('위젯을 상하로 배치하기')),
+        backgroundColor: Color(0xFF006090),
+        foregroundColor: Colors.white,
+      ),
+      body: Body()
+    )
   ));
 }
 
-class CustomContainer extends StatelessWidget {
-  const CustomContainer({super.key});
+class Body extends StatelessWidget {
+  const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: 100,
-        height: 100,
-        decoration: BoxDecoration(
-            color: Color(0x90013429),
-            border: Border.all(
-                color: Colors.red, width: 5, style: BorderStyle.solid),
-            borderRadius: BorderRadius.circular(20),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.3),
-                offset: Offset(6, 10),
-                blurRadius: 10,
-                spreadRadius: 10
-              )
-            ]),
-        child: Center(child: Text('Container')));
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(width: 100, height: 20, color: Colors.red, child: Text('Container 1')),
+        Container(width: 100, height: 20, color: Colors.green, child: Text('Containter 2')),
+        Container(width: 100, height: 20, color: Colors.yellow, child: Text('Container 3'))
+      ],
+    );
   }
 }
