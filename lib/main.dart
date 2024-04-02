941829e9baa535c399cba2420d5599ecff2b70fa
minyeong <minyk0512@gmail.com>
2024-04-03 02:58:08 +0900

    Row, Scroll view 연습

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,13 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('위젯을 상하로 배치하기')),
-        backgroundColor: Color(0xFF006090),
-        foregroundColor: Colors.white,
-      ),
-      body: Body()
-    )
-  ));
+      home: Scaffold(
+          appBar: AppBar(
+            title: Center(child: Text('위젯을 좌우로 배치하기')),
+            backgroundColor: Color(0xFF006090),
+            foregroundColor: Colors.white,
+          ),
+          body: Body())));
 }
 
 class Body extends StatelessWidget {
@@ -18,15 +16,36 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Container(width: 100, height: 20, color: Colors.red, child: Text('Container 1')),
-        Container(width: 100, height: 20, color: Colors.green, child: Text('Containter 2')),
-        Container(width: 100, height: 20, color: Colors.yellow, child: Text('Container 3'))
-      ],
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+          children: [
+            Container(
+                width: 100,
+                height: 50,
+                color: Colors.red,
+                margin: EdgeInsets.symmetric(horizontal: 8),
+                child: Text('Container 1')),
+            Container(
+                width: 100,
+                height: 50,
+                color: Colors.green,
+                margin: EdgeInsets.symmetric(horizontal: 8),
+                child: Text('Containter 2')),
+            Container(
+                width: 100,
+                height: 50,
+                color: Colors.yellow,
+                margin: EdgeInsets.symmetric(horizontal: 8),
+                child: Text('Container 3')),
+            Container(
+            width: 100,
+                height: 50,
+              color: Colors.blue,
+              margin: EdgeInsets.symmetric(horizontal: 8),
+              child: Text('Container 4')
+            )
+          ]),
     );
   }
 }
