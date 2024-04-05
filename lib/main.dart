d0c46f7c4cf60e144c038a14a1a2cc05e184b243
minyeong <minyk0512@gmail.com>
2024-04-06 02:52:26 +0900

    Stateful Widget, Stateless Widget 비교

--- lib/main.dart
+++ lib/main.dart
@@ -4,50 +4,73 @@ import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Center(child: Text('위젯을 쌓아올리기')),
-            backgroundColor: Color(0xFF006090),
-            foregroundColor: Colors.white,
-          ),
-          body: Body())));
+  runApp(MaterialApp(home: Scaffold(body: HomeWidget())));
 }
 
-class Body extends StatelessWidget {
-  const Body({super.key});
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.center,
-      child: Container(
-        decoration: BoxDecoration(color: Colors.black),
-        child: Stack(
-          children: [
-            Align(
-              alignment: Alignment.center,
-              child: Container(
-                  width: 300,
-                  height: 300,
-                  decoration: BoxDecoration(
-                      color: Colors.red, borderRadius: BorderRadius.circular(150))),
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: Container(
-                  width: 280,
-                  height: 280,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(140))),
-            ),
-            Align(
-              alignment: Alignment.center,
-                child: const Text('Count',
-                    style: TextStyle(color: Colors.red, fontSize: 32))),
-          ],
-        ),
+    return Column(children: [ExampleStateless(), ExampleStateful(index: 3)]);  // ExampleStateful의 index를 초기값 3으로 호출
+  }
+}
+
+class ExampleStateless extends StatelessWidget {
+  const ExampleStateless({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(color: Colors.red),
+    );
+  }
+}
+
+class ExampleStateful extends StatefulWidget {
+  final int index;
+  const ExampleStateful({required this.index, super.key});
+
+  @override
+  State<ExampleStateful> createState() => _ExampleStatefulState();
+}
+
+class _ExampleStatefulState extends State<ExampleStateful> {
+  late int _index;  // index가 null이어도 되는 상황이라면 int? index;로 해도 가능함
+  late TextEditingController textController;
+
+  @override
+  void initState() {
+    super.initState();
+    _index = widget.index;
+    textController = TextEditingController();
+  }
+
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            if (_index == 5) {
+              _index = 0;
+              return;
+            }
+
+            _index++;
+          });
+        },
+        child:
+            Container(color: Colors.blue.withOpacity(_index/5), child: Center(child: Text('$_index'))),
       ),
     );
   }
