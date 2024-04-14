a6df0c5c48dfb04ec445f03261b45c02aec37cc6
minyeong <minyk0512@gmail.com>
2024-04-15 03:07:13 +0900

    외부라이브러리 사용해보기

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,15 @@
-import 'package:flutter/cupertino.dart';
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
-    body: const Body(),
-    appBar: AppBar(
-      title: const Text('CallBack 공부하기'),
-      centerTitle: true,
-      backgroundColor: Colors.black,
-      foregroundColor: Colors.amberAccent,
-    ),
-  )));
+          appBar: AppBar(
+              title: Text('외부라이브러리 사용해보기'),
+              backgroundColor: Colors.deepPurpleAccent,
+              foregroundColor: Colors.white,
+              centerTitle: true),
+          body: const Body())));
 }
 
 class Body extends StatelessWidget {
@@ -20,55 +17,22 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TestWidget();
+    return RandomWords();
   }
 }
 
-class TestWidget extends StatefulWidget {
-  const TestWidget({super.key});
-
-  @override
-  State<TestWidget> createState() => _TestWidgetState();
-}
-
-class _TestWidgetState extends State<TestWidget> {
-  int value = 0;
+class RandomWords extends StatelessWidget {
+  const RandomWords({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Text('Count: $value', style: const TextStyle(fontSize: 30)),
-      TestButton(AddCounter)
-    ]);
-  }
-
-  // 'Up Counter' (Container) 입력시 실행할 콜백함수
-  void AddCounter(int addValue) => setState(() => value += addValue);
-}
-
-class TestButton extends StatelessWidget {
-  const TestButton(this.callback, {super.key});
-
-  final Function(int) callback;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => callback.call(1),
-      onDoubleTap: () => callback.call(5),
-      onLongPress: () => callback.call(10),
-      child: Container(
-          margin: const EdgeInsets.symmetric(vertical: 8),
-          width: double.infinity,
-          child: Center(
-              child: Container(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                  decoration: BoxDecoration(border: Border.all()),
-                  child: const Text(
-                    'Up Counter',
-                    style: TextStyle(fontSize: 24),
-                  )))),
+    final wordList = generateWordPairs().take(5).toList();
+    final widgets = wordList
+        .map((word) =>
+            Text(word.asCamelCase, style: const TextStyle(fontSize: 32)))
+        .toList();
+    return Column(
+      children: widgets,
     );
   }
 }
