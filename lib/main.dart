cbaa6b4004a65cc552003a14d665473fe5891b32
minyeong <minyk0512@gmail.com>
2024-04-16 02:26:41 +0900

    StatefulWidget 활용하여 화면전환 연출하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,56 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-const assetImagePath = 'assets/images';
-const samplePath = '$assetImagePath/cat.png';
-
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-            title: Text('로컬데이터 활용하기'),
-            backgroundColor: Colors.deepPurpleAccent,
-            foregroundColor: Colors.white,
-            centerTitle: true),
-      body: Body())));
+  runApp(const MaterialApp(home: HomeWidget()));
 }
 
-class Body extends StatelessWidget {
-  const Body({super.key});
+class HomeWidget extends StatefulWidget {
+  const HomeWidget({super.key});
+
+  @override
+  State<HomeWidget> createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  late int index;
+
+  @override
+  void initState() {
+    super.initState();
+    index = 0;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Image.asset(samplePath));
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text('Flutter 화면 이동하기'),
+          backgroundColor: Colors.deepPurpleAccent,
+          foregroundColor: Colors.white,
+          centerTitle: true),
+      body: homeBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          const BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+          const BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
+          const BottomNavigationBarItem(icon: Icon(Icons.person), label: 'person')
+        ],
+        currentIndex: index,
+        onTap: (newIndex) => setState(() => index = newIndex)
+      ),
+    );
+  }
+
+  Widget homeBody() {
+    switch (index) {
+      case 1:
+        return const Center(child: Icon(Icons.search, size: 100));
+      case 2:
+        return const Center(child: Icon(Icons.person, size: 100));
+      case 0:
+      default:
+        return const Center(child: Icon(Icons.home, size: 100));
+    }
   }
 }
-
